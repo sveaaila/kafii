e2d8bf7d3bf2f7ad5345361296988b43cee038a9
bfeitknecht <bfeitknecht@ethz.ch>
2025-09-11 16:15:13 +0200

    Quartz sync: Sep 11, 2025, 4:15 PM

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "development flake for svea's blog";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }:
+    let
+      system = "aarch64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+           nodejs_24
+           git
+        ];
+      };
+    };
+}
